a25fe681fde2604211d32edcaf50e4268cbd145b
eransa <eransafadel@gmail.com>
2022-06-08 00:46:13 +0300

    after redux

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,6 +2,7 @@ import React,{useState} from "react";
 import useStyles from "./styles";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64'; 
+//hey
 
 export default function Form() {
   const classes = useStyles();
